a9ce4a160c723df96224907f63a5c9cc62d8c562
carbomint <tranquocthienbao12@gmail.com>
2026-05-16 23:12:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 // Discord Status Configuration
+window.siteConfig = {
 identity: {
-    name:     'carbomint'
-    discord:  '920842715351113728'
-    avatar:   'https://cdn.discordapp.com/attachments/1388482649655607328/1505241185130643548/wqj75jh.png?ex=6a09e898&is=6a089718&hm=4c4e5e42fb96a7d28aec95d947213d52aa23e2c4feed38004d3937931486e253'
-}
+    name:     'carbomint',
+    discord:  '920842715351113728',
+    avatar:   'https://cdn.discordapp.com/attachments/1388482649655607328/1505241185130643548/wqj75jh.png?ex=6a09e898&is=6a089718&hm=4c4e5e42fb96a7d28aec95d947213d52aa23e2c4feed38004d3937931486e253',
+},
 const DISCORD_CONFIG = {
     userId: "920842715351113728",
     apiEndpoint: "https://api.lanyard.rest/v1/users/920842715351113728"
