ec1d334f5c2d5e9ac0c9c210dcd2a2faa7a48f97
carbomint <tranquocthienbao12@gmail.com>
2026-05-16 22:43:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Discord Status Configuration
 const DISCORD_CONFIG = {
-    userId: '920842715351113728',
-    apiEndpoint: 'https://api.lanyard.rest/v1/users/920842715351113728'
+    userId: "920842715351113728",
+    apiEndpoint: "https://api.lanyard.rest/v1/users/920842715351113728"
 };
